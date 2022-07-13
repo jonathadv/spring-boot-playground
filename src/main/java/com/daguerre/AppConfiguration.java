118f4e5f3f45d4dd595d9b95de1a2882ef3196f5
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2022-07-12 21:27:03 -0300

    Set AppConfiguration to run only with @Profile("dev")

--- src/main/java/com/daguerre/AppConfiguration.java
+++ src/main/java/com/daguerre/AppConfiguration.java
@@ -1,10 +1,19 @@
 package com.daguerre;
 
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class AppConfiguration {
 
-
+    @Bean
+    public CommandLineRunner run(){
+        return args -> {
+            System.out.println("#### Running development profile ####");
+        };
+    }
 }
