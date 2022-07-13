5d2a0658517b8e289c087c0a742ca115846f86dd
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2022-07-12 21:14:43 -0300

    Add application.yml

--- src/main/java/com/daguerre/AppConfiguration.java
+++ src/main/java/com/daguerre/AppConfiguration.java
@@ -1,19 +1,10 @@
 package com.daguerre;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sales System";
-    }
 
-    @Bean(name = "applicationVersion")
-    public String applicationVersion(){
-        return "1.0";
-    }
 }
--- src/main/java/com/daguerre/Application.java
+++ src/main/java/com/daguerre/Application.java
@@ -1,7 +1,6 @@
 package com.daguerre;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
-    @Autowired
-    @Qualifier("applicationName")
-    String applicationName;
+    @Value("${application.name}")
+    private String applicationName;
 
-    @Autowired
-    @Qualifier("applicationVersion")
-    String applicationVersion;
+    @Value("${application.version}")
+    private String applicationVersion;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
