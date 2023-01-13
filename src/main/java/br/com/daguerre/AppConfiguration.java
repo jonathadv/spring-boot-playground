f4fafeebbac6169040135b5e217bf7a0de18d70e
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2023-01-12 21:57:48 -0300

    setup logs

--- src/main/java/br/com/daguerre/AppConfiguration.java
+++ src/main/java/br/com/daguerre/AppConfiguration.java
@@ -6,11 +6,14 @@ import br.com.daguerre.service.ClientService;
 import br.com.daguerre.service.ProductService;
 import java.math.BigDecimal;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
 @DevConfiguration
 public class AppConfiguration {
+  private static final Logger LOGGER = LoggerFactory.getLogger(AppConfiguration.class);
   private ClientService clientService;
   private ProductService productService;
 
@@ -22,14 +25,16 @@ public class AppConfiguration {
   @Bean
   public CommandLineRunner run() {
     return args -> {
-      System.out.println("#### Adding values to Database ####");
+      LOGGER.info("#### Adding values to Database ####");
 
+      LOGGER.info("Creating clients");
       clientService.saveAll(
           List.of(
               ClientEntity.builder().name("John").build(),
               ClientEntity.builder().name("Lucas").build(),
               ClientEntity.builder().name("Dani").build()));
 
+      LOGGER.info("Creating products");
       productService.saveAll(
           List.of(
               ProductEntity.builder().description("Shirt").price(BigDecimal.valueOf(10.50)).build(),
